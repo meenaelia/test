1e1522e0c5da1b2c8c42722e6ea28360bfce447c
Mina <Mina@Mina-PC>
2018-10-26 14:32:35 +0200

    init

---
+++ jenkinsintro/src/test/java/test/test.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class test {
+	@Test(priority=0)
+	public void testRegestraion()
+	{
+	
+		Assert.assertTrue(true);
+}
+}
---
+++ jenkinsintro/src/test/java/test/test1.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class test1 {
+	
+@Test
+public void testcase1(){
+
+	Assert.assertTrue(true);
+}
+
+}
